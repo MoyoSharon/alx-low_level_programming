aeaf30747343aab366699a53cc27bd46b4d618dd
MoyoSharon <mofesharon61@gmail.com>
2023-03-26 05:13:08 -0700

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,12 @@
 
 void print_line(int n)
 {
-	if (n > 0)
-	{
-		int i;
+	int i;
 
-		for (int i = 0; i < n; i++)
-			_putchar('_');
-		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
 	}
-	else
 		_putchar('\n');
 }
+
